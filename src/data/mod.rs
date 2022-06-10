526b7daebbf5c6eb9af53f2cd639e20df6a35c35
Jörg Sischka <j.sischka@kochmedia.com>
2022-06-10 17:36:02 +0200

    comments

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -1,4 +1,3 @@
-// pub use self::csv_column;
 pub mod column;
 pub mod data;
 pub mod value;
--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -1,9 +1,10 @@
-pub mod parser_config;
-// pub mod from_json_config;
 pub mod iterating_parser;
+pub mod line_tokenizer; // works, tested
+pub mod parser_common;
+pub mod parser_config; // no tests, just struct definitions
+pub mod skip_take_lines; // works, tested
+pub mod transform_sanitize_token; // works, tested
+
 // pub mod line_tokenizer_OLD;
-pub mod line_tokenizer;
+// pub mod from_json_config;
 // pub mod mem_parser;
-// pub mod parser_common;
-pub mod skip_take_lines;
-pub mod transform_sanitize_token;
