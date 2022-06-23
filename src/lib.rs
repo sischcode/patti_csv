086534c07518a7d5402fbb66626b0003eb5dc236
Jörg Sischka <j.sischka@kochmedia.com>
2022-06-23 14:16:01 +0200

    done

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
-#[allow(dead_code)]
-mod data;
-#[allow(dead_code)]
-mod errors;
-#[allow(dead_code)]
-mod parse;
+pub mod errors;
+pub mod iterating_parser;
+pub mod line_tokenizer; // works, tested
+pub mod parser_common; // works, tested
+pub mod parser_config; // no tests, just struct definitions
+pub mod skip_take_lines; // works, tested
+pub mod transform_sanitize_token; // works, tested
